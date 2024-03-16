50adb41d0fac731b3387e3562becaa093e43f42f
Dânison dos Santos <danison.santos00@gmail.com>
2024-03-15 21:36:36 -0300

    feat: add User class

---
+++ src/main/java/com/dan/model/User.java
@@ -0,0 +1,61 @@
+package com.dan.model;
+
+import java.util.Date;
+
+public class User {
+
+    private static int idManager = 0;
+    private final int id;
+    private String name;
+    private String cpf;
+    private int age;
+    private Date birthDate;
+
+    public User(String name, String cpf, int age, Date birthDate) {
+        this.id = idManager = idManager++;
+        this.name = name;
+        this.cpf = cpf;
+        this.age = age;
+        this.birthDate = birthDate;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+
+}
