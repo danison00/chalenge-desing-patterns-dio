04fc8f9d2b0fb8336f9e03d594e3a0a666892d9c
Dânison dos Santos <danison.santos00@gmail.com>
2024-03-15 23:08:01 -0300

    feat: add NoAvailableCopiesExceptions

---
+++ src/main/java/com/dan/model/exception/NoAvailableCopiesExceptions.java
@@ -0,0 +1,8 @@
+package com.dan.model.exception;
+
+public class NoAvailableCopiesExceptions extends RuntimeException{
+
+    public NoAvailableCopiesExceptions(String message) {
+        super(message);
+    }
+}
