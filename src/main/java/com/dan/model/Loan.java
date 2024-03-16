8ae2ca96fdc2429e64f24fa456baae83aa85e323
Dânison dos Santos <danison.santos00@gmail.com>
2024-03-15 21:45:43 -0300

    feat: add Loan class

---
+++ src/main/java/com/dan/model/Loan.java
@@ -0,0 +1,36 @@
+package com.dan.model;
+
+import java.time.LocalDateTime;
+
+public class Loan {
+
+
+    private static int idManager = 0;
+    private final int id;
+    private final Book book;
+    private final User user;
+    private final LocalDateTime dateTime;
+
+    public Loan(Book book, User user) {
+        this.id = idManager = idManager++;
+        this.book = book;
+        this.user = user;
+        this.dateTime = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+}
