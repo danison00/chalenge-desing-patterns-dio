78bc79b74d3759fb55a8c6e73fbc4571dd99bc9e
Dânison dos Santos <danison.santos00@gmail.com>
2024-03-16 11:26:37 -0300

    feat: add NotificationPreferences.class

---
+++ src/main/java/com/dan/model/entities/NotificationPreferences.java
@@ -0,0 +1,12 @@
+package com.dan.model.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotificationPreferences {
+
+    private boolean enable;
+    private boolean smsEnable;
+    private boolean emailEnable;
+
+}
