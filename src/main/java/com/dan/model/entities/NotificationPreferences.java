69c45303b6114e0db85e28853278866f9db8e209
Dânison dos Santos <danison.santos00@gmail.com>
2024-03-16 11:33:20 -0300

    up: add methods boilerplate

--- src/main/java/com/dan/model/entities/NotificationPreferences.java
+++ src/main/java/com/dan/model/entities/NotificationPreferences.java
@@ -9,4 +9,42 @@ public class NotificationPreferences {
     private boolean smsEnable;
     private boolean emailEnable;
 
+    public NotificationPreferences(boolean enable, boolean smsEnable, boolean emailEnable) {
+        this.enable = enable;
+        this.smsEnable = smsEnable;
+        this.emailEnable = emailEnable;
+    }
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    public boolean isSmsEnable() {
+        return smsEnable;
+    }
+
+    public void setSmsEnable(boolean smsEnable) {
+        this.smsEnable = smsEnable;
+    }
+
+    public boolean isEmailEnable() {
+        return emailEnable;
+    }
+
+    public void setEmailEnable(boolean emailEnable) {
+        this.emailEnable = emailEnable;
+    }
+
+    @Override
+    public String toString() {
+        return "NotificationPreferences{" +
+                "enable=" + enable +
+                ", smsEnable=" + smsEnable +
+                ", emailEnable=" + emailEnable +
+                '}';
+    }
 }
