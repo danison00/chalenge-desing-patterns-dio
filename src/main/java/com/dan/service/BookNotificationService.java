f71713effef0e3b20a3c8b1d9a88d04d7a01a0a2
Dânison dos Santos <danison.santos00@gmail.com>
2024-03-16 17:00:52 -0300

    up: implements method notify

--- src/main/java/com/dan/service/BookNotificationService.java
+++ src/main/java/com/dan/service/BookNotificationService.java
@@ -1,13 +1,31 @@
 package com.dan.service;
 
 import com.dan.model.entities.User;
+import com.dan.service.interfaces.BookNotificationStrategy;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookNotificationService {
 
 
-    public void notify(List<User> users) {
-        users.forEach(user -> System.out.printf("notificando user: %s", user.getName()));
+    public void notify(List<User> users, String message) {
+
+        System.out.println("\n **** Notificando Usuários ****");
+
+
+        users.forEach(u -> {
+           var m = new StringBuilder("Olá, ");
+            m.append(u.getName()).append("! ").append(message);
+
+            if(u.getNotificationPreferences().isEmailEnable())
+                BookNotificationByEmailService.getInstance().notify(u.getEmail(), m.toString());
+
+            if (u.getNotificationPreferences().isSmsEnable())
+                BookNotificationBySmsService.getInstance().notify(u.getPhone(), m.toString());
+        });
+
+        System.out.println();
     }
+
 }
