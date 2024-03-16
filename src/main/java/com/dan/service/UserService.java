47f511db84692d591757b7240898d2679f98168b
Dânison dos Santos <danison.santos00@gmail.com>
2024-03-16 14:30:23 -0300

    refact: UserService extends AbstractService

--- src/main/java/com/dan/service/UserService.java
+++ src/main/java/com/dan/service/UserService.java
@@ -8,16 +8,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class UserService implements Observer {
+public class UserService extends AbstractService<User> implements Observer {
 
-    private final List<User> users = new ArrayList<>();
     private final BookNotificationService bookNotificationService = new BookNotificationService();
 
     @Override
     public void notify(String message) {
-        var usersToNotify = users.stream().filter(u -> u.getNotificationPreferences().isEnable()).toList();
+        var usersToNotify = super.getList().stream().filter(u -> u.getNotificationPreferences().isEnable()).toList();
 
         bookNotificationService.notify(usersToNotify);
+    }
+
+    @Override
+    protected void afterCreater(User user) {
+
     }
 
     static class UserServiceHolder {
@@ -32,31 +36,7 @@ public class UserService implements Observer {
         return UserServiceHolder.instance;
     }
 
-    public void create(User user) {
-        this.users.add(user);
 
-        System.out.println("Usuário criado com sucesso!");
-    }
-
-    public void deleteById(int id) throws NotFoundException {
-        findById(id);
-        this.users.removeIf(u -> u.getId() == id);
-
-    }
-
-    public User findById(int id) throws NotFoundException {
-        for (var u : users)
-            if (u.getId() == id)
-                return u;
-
-        throw new RuntimeException("Usuário não encontrado");
-    }
-
-    public void list() {
-        users.forEach((u) -> {
-            System.out.println(u.toString());
-        });
-    }
 
 
 }
