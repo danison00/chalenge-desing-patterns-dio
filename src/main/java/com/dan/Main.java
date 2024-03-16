93c04f25ce31a01dad31a973261ccf64e9698353
Dânison dos Santos <danison.santos00@gmail.com>
2024-03-15 23:49:16 -0300

    fix: clean Main.class

--- src/main/java/com/dan/Main.java
+++ src/main/java/com/dan/Main.java
@@ -1,7 +1,16 @@
 package com.dan;
 
+import com.dan.model.entities.Book;
+import com.dan.model.entities.User;
+import com.dan.model.exception.NotFoundException;
+import com.dan.service.BookService;
+import com.dan.service.LoanService;
+import com.dan.service.UserService;
+
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new System().run();
     }
 }
