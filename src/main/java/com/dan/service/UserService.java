dbef69911feb376ac874f84bec4e4800e3b31756
Dânison dos Santos <danison.santos00@gmail.com>
2024-03-16 13:12:36 -0300

    up: implements observer and add BookNotificationService dependency

--- src/main/java/com/dan/service/UserService.java
+++ src/main/java/com/dan/service/UserService.java
@@ -2,19 +2,30 @@ package com.dan.service;
 
 import com.dan.model.entities.User;
 import com.dan.model.exception.NotFoundException;
+import com.dan.model.observer.Observer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
-public class UserService {
+public class UserService implements Observer {
 
     private final List<User> users = new ArrayList<>();
+    private final BookNotificationService bookNotificationService = new BookNotificationService();
+
+    @Override
+    public void notify(String message) {
+        var usersToNotify = users.stream().filter(u -> u.getNotificationPreferences().isEnable()).toList();
+
+        bookNotificationService.notify(usersToNotify);
+    }
 
     static class UserServiceHolder {
         private static final UserService instance = new UserService();
     }
 
     private UserService() {
+        BookService.getInstance().subscribe(this);
     }
 
     public static UserService getInstance() {
@@ -23,6 +34,7 @@ public class UserService {
 
     public void create(User user) {
         this.users.add(user);
+
         System.out.println("Usuário criado com sucesso!");
     }
 
